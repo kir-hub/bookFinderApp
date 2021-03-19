ec61c7c9fc3ba7ea3c24f56120cc450dea08bfd1
kir-hub <kirill.silin.12321@gmail.com>
2021-03-19 16:44:55 +0200

    init

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,7 @@
 import React from 'react'
-import axios from 'axios'
 
 export default function App() {
-    console.log(1);
-    const res = axios.get('https://www.googleapis.com/books/v1/volumes/zyTCAlFPjgYC?projection=lite&key=AIzaSyDQR90V3fE8VfBnUYlH6T8UCkBWcRcgNaM')
-    console.log(res);
+    
 
     return (
         <div>
