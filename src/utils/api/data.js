dd1f4146a16462bae2c0c9e179bbd3e13cc14981
kir-hub <kirill.silin.12321@gmail.com>
2021-03-22 13:03:45 +0200

    removeed comments from data.js

--- src/utils/api/data.js
+++ src/utils/api/data.js
@@ -2,11 +2,7 @@ import axios from 'axios'
 import apiKey from '../../../key'
 
 const BASE_URL = 'https://www.googleapis.com/books/v1/volumes?q='
-const BOOK_TITLE = 'string'
-const AUTHOR = `+inauthor:${name}`//добавить проверку на наличие нескольких условий
 const KEY = `&key=&key=${apiKey}`
 
-// const getBooks = (book_title, author) => axios.get(`https://www.googleapis.com/books/v1/volumes/zyTCAlFPjgYC?projection=lite&key=${KEY}`)
-// const getBooks = (book_title, author) => axios.get(`${BASE_URL}${book_title}${author && AUTHOR + author}${KEY}`)
 const getBooks = (book_title, author) => axios.get(`${BASE_URL}${book_title}${author && '+inauthor:'+author}${KEY}`)
 export default getBooks;
